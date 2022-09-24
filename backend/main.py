a1fb0110f5858463bd4d7909412815e4bc43bc27
Dylan Oldfield <dylan.oldfield@yahoo.com>
2022-09-24 14:06:40 +1000

    updated host example

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from models.shared import db
 from models.cars import CarsModel
+from models.host import HostModel
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
