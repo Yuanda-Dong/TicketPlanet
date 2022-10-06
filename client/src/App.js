4b0bf714949ce5eda6e665753c1918603d1bce07
Serena71 <serena991031zs@gmail.com>
2022-10-06 14:38:34 +1100

    edit router path

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import Landing from './pages/Landing/Landing';
 import SearchPage from './pages/SearchPage/SearchPage';
+import SignInSide from './pages/Signin';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Navbar from './components/Navbar/NavBar';
 
 function App() {
   return (
     <BrowserRouter>
+      {/* <Navbar /> */}
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/search" element={<SearchPage />} />
+        <Route path="/">
+          <Route index element={<Landing />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="signin" element={<SignInSide />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
