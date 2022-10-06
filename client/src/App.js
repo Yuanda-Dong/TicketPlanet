eee45f29838d53d7ce6ad10a1e2cef7f44bccc87
Serena71 <serena991031zs@gmail.com>
2022-10-06 23:36:48 +1100

    fix route for payment

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,9 @@ import Landing from './pages/Landing/Landing';
 import SearchPage from './pages/SearchPage/SearchPage';
 import SignInSide from './pages/Signin';
 import SignUp from './pages/SignUp';
-import EventDetail from './components/EventDetail/EventDetail';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Payment from "./pages/Payment/Payment";
+import EventDetail from './pages/EventDetail/EventDetail';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Payment from './pages/Payment/Payment';
 // import Navbar from './components/Navbar/NavBar';
 
 function App() {
@@ -19,10 +19,9 @@ function App() {
           <Route path="signin" element={<SignInSide />} />
           <Route path="signup" element={<SignUp />} />
           <Route path="event">
-            <Route path=":id" element={<EventDetail />}>
-              <Route path="purchase" element={<Payment/>}/>
-            </Route>
+            <Route path=":id" element={<EventDetail />} />
           </Route>
+          <Route path="payment" element={<Payment />} />
         </Route>
       </Routes>
     </BrowserRouter>
