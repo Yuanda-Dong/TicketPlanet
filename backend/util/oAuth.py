f2c25a614d3730c512dc935d8deee222377e384a
Dylan Oldfield <dylan.oldfield@yahoo.com>
2022-10-11 20:56:21 +1100

    testing goog auth

--- backend/util/oAuth.py
+++ backend/util/oAuth.py
@@ -11,17 +11,17 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 #Google Auth
-config = Config('.env')
-oauth = OAuth(config)
+# config = Config('.env')
+# oauth = OAuth(config)
 
-CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
-oauth.register(
-    name='google',
-    server_metadata_url=CONF_URL,
-    client_kwargs={
-        'scope': 'openid email profile'
-    }
-)
+# CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+# oauth.register(
+#     name='google',
+#     server_metadata_url=CONF_URL,
+#     client_kwargs={
+#         'scope': 'openid email profile'
+#     }
+# )
 
 
 def verify_password(plain_password, hashed_password):
