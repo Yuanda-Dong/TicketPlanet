7e6a3815bdb0e3bed6789d0e18cbc08b9311c9ee
Serena71 <serena991031zs@gmail.com>
2022-10-11 15:56:15 +1100

    add real name in signup form

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -9,20 +9,20 @@ import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
-import {SignUpForm1, SignUpForm2} from '../components/SignUp/SignUpForm';
-import {useNavigate} from 'react-router-dom';
+import { SignUpForm1, SignUpForm2 } from '../components/SignUp/SignUpForm';
+import { useNavigate } from 'react-router-dom';
 import NavBar from '../components/Navbar/NavBar';
 import GoogleIcon from '@mui/icons-material/Google';
-import './Payment/Payment.css'
+import './Payment/Payment.css';
 
 const steps = ['Register your account', 'Enter your details'];
 
 function getStepContent(step, [state1, state2]) {
   switch (step) {
     case 0:
-      return <SignUpForm1 state={state1}/>;
+      return <SignUpForm1 state={state1} />;
     case 1:
-      return <SignUpForm2 state={state2}/>;
+      return <SignUpForm2 state={state2} />;
     default:
       throw new Error('Unknown step');
   }
@@ -36,9 +36,8 @@ export default function SignUp() {
     navigate('/signin');
   };
 
-  const [signupInfo, setSignupInfo] = React.useState({ email: '', password: '' });
+  const [signupInfo, setSignupInfo] = React.useState({ firstname: '', lastname: '', email: '', password: '' });
   const [profileInfo, setProfileInfo] = React.useState({ gender: '', age: '', postcode: '' });
-
 
   const handleNext = (e) => {
     setActiveStep(activeStep + 1);
@@ -51,73 +50,72 @@ export default function SignUp() {
   return (
     <div>
       <NavBar />
-      <div className='Signs'>
-      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-          <Typography component="h1" variant="h4" align="center">
-            Sign Up Account
-          </Typography>
-          <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
-            {steps.map((label) => (
-              <Step key={label}>
-                <StepLabel>{label}</StepLabel>
-              </Step>
-            ))}
-          </Stepper>
-          <React.Fragment>
-            {activeStep === steps.length ? (
-              <React.Fragment>
-                <Typography variant="h5" gutterBottom>
-                  Congrats! You've just created your account.
-                </Typography>
-                <Typography variant="subtitle1">
-                  You can click{' '}
-                  <span style={{ color: 'blue', textDecoration: 'underline' }} onClick={navSignIn}>
-                    here
-                  </span>{' '}
-                  to login.
-                </Typography>
-                <Typography variant="subtitle1"></Typography>
-              </React.Fragment>
-            ) : (
-              <React.Fragment>
-                {getStepContent(activeStep, [
-                  [signupInfo, setSignupInfo],
-                  [profileInfo, setProfileInfo],
-                ])}
-                <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                  {activeStep !== 0 && (
-                    <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
-                      Back
+      <div className="Signs">
+        <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+          <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+            <Typography component="h1" variant="h4" align="center">
+              Sign Up Account
+            </Typography>
+            <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+              {steps.map((label) => (
+                <Step key={label}>
+                  <StepLabel>{label}</StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+            <React.Fragment>
+              {activeStep === steps.length ? (
+                <React.Fragment>
+                  <Typography variant="h5" gutterBottom>
+                    Congrats! You've just created your account.
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    You can click{' '}
+                    <span style={{ color: 'blue', textDecoration: 'underline' }} onClick={navSignIn}>
+                      here
+                    </span>{' '}
+                    to login.
+                  </Typography>
+                  <Typography variant="subtitle1"></Typography>
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  {getStepContent(activeStep, [
+                    [signupInfo, setSignupInfo],
+                    [profileInfo, setProfileInfo],
+                  ])}
+                  <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    {activeStep !== 0 && (
+                      <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+                        Back
+                      </Button>
+                    )}
+                    <Button variant="contained" onClick={handleNext} sx={{ mt: 3, ml: 1 }}>
+                      {activeStep === steps.length - 1 ? 'Sign Up' : 'Next'}
                     </Button>
-                  )}
-                  <Button variant="contained" onClick={handleNext} sx={{ mt: 3, ml: 1 }}>
-                    {activeStep === steps.length - 1 ? 'Sign Up' : 'Next'}
+                  </Box>
+                </React.Fragment>
+              )}
+            </React.Fragment>
+          </Paper>
+          {activeStep === 0 && (
+            <Box>
+              <Grid container justifyContent="space-between" alignItems="center">
+                <Grid item>
+                  <Button startIcon={<GoogleIcon />} variant="outlined">
+                    Sign up with Google
                   </Button>
-                </Box>
-              </React.Fragment>
-            )}
-          </React.Fragment>
-        </Paper>
-        {activeStep === 0 && (
-          <Box>
-            <Grid container justifyContent="space-between" alignItems="center">
-              <Grid item>
-                <Button startIcon={<GoogleIcon />} variant="outlined">
-                  Sign up with Google
-                </Button>
+                </Grid>
+                <Grid item>
+                  <Link variant="body2" onClick={navSignIn}>
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Link variant="body2" onClick={navSignIn}>
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        )}
-      </Container>
-        </div>
+            </Box>
+          )}
+        </Container>
+      </div>
     </div>
   );
 }
-
