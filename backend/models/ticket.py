7df0672f38fe7e52413ec06358d76b28986fcc58
Dylan Oldfield <dylan.oldfield@yahoo.com>
2022-10-15 14:09:16 +1100

    added tickets updates not working

--- backend/models/ticket.py
+++ backend/models/ticket.py
@@ -5,7 +5,25 @@ from pydantic import BaseModel,Field
 
 class Ticket(BaseModel):
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
-    eventId: str
     ticket_name :str = Field(...)
     price: float
     availability: int
+    
+    class Config:
+        allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "ticket_name": "General Admission",
+                "price": "15",
+                "availability":"55"
+            }
+        }
+
+class TicketInDB(Ticket):
+    event_id: str
+    host_id: str
+    
+class TicketUpdate(BaseModel):
+    ticket_name :Optional[str]
+    price: Optional[float]
+    availability: Optional[int]
