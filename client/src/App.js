2a274284a1c9fdd674f87fdf4895c3a08d2eed71
Serena71 <serena991031zs@gmail.com>
2022-10-20 12:21:40 +1100

    add axiosInstance for payment page (commentted out)

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,11 @@ import SignInSide from './pages/Signin';
 import SignUp from './pages/SignUp';
 import EventDetail from './pages/EventDetail/EventDetail';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MyTickets from "./pages/MyTickets/MyTickets";
+import MyTickets from './pages/MyTickets/MyTickets';
 import Dashboard from './pages/Dashboard/Dashboard.jsx';
 import Events from './components/Dashboard/Events';
 import Reports from './components/Dashboard/Reports';
-// import Payment from './pages/Payment/Payment';
-// import Navbar from './components/Navbar/NavBar';
+import Payment from './pages/Payment/Payment';
 import Footer from './components/Footer/Footer';
 import PostPage from './pages/PostPage/PostPage';
 import EditPage from './pages/EditPage/EditPage';
@@ -37,8 +36,7 @@ function App() {
             <Route path="events" element={<Events />} />
             <Route path="reports" element={<Reports />} />
           </Route>
-          {/* <Route path="payment" element={<Payment />} />
-           */}
+          {/* <Route path="payment" element={<Payment />} /> */}
           <Route path="post" element={<PostPage />} />
           <Route path="edit" element={<EditPage />} />
           <Route path="my-tickets" element={<MyTickets />} />
--- client/src/pages/Payment/Payment.jsx
+++ client/src/pages/Payment/Payment.jsx
@@ -4,6 +4,7 @@ import './Payment.css';
 import React, { useState, useEffect } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
+import { axiosInstance } from '../../config';
 
 const stripePromise = loadStripe(
   'pk_test_51LplrMEHt2rYmWMjSn2QxFV529CtYWqLfN26Xpj8QLcZaOBKfxeg6YYGMDhg3TX2mYRm7SR0JK9vtLD9nbYR4x1R00gJqzzD6E'
@@ -12,16 +13,19 @@ const stripePromise = loadStripe(
 export default function Payment() {
   const [clientSecret, setClientSecret] = useState('');
 
-  // useEffect(() => {
-  //     // Create PaymentIntent as soon as the page loads
-  //     fetch("/create-payment-intent", {
-  //         method: "POST",
-  //         headers: {"Content-Type": "application/json"},
-  //         body: JSON.stringify({items: [{id: "xl-tshirt"}]}),
-  //     })
-  //         .then((res) => res.json())
-  //         .then((data) => setClientSecret(data.clientSecret));
-  // }, []);
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    // fetch('/create-payment-intent', {
+    //   method: 'POST',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify({ items: [{ id: 'xl-tshirt' }] }),
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => setClientSecret(data.clientSecret));
+    // axiosInstance
+    //   .post('/create-payment-intent', { items: [{ id: 'xl-tshirt' }] })
+    //   .then((res) => setClientSecret(res.data.clientSecret));
+  }, []);
 
   const appearance = {
     theme: 'stripe',
