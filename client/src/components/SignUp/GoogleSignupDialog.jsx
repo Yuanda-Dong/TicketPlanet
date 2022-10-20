f9dd1a151758d7a06a9ad6d3f435d180c37db207
Serena71 <serena991031zs@gmail.com>
2022-10-21 00:52:35 +1100

    finish google signin/up api integration

--- client/src/components/SignUp/GoogleSignupDialog.jsx
+++ client/src/components/SignUp/GoogleSignupDialog.jsx
@@ -6,9 +6,33 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
 import { SignUpForm2 } from './SignUpForm';
+import { useDispatch } from 'react-redux';
+import { axiosInstance } from '../../config';
+import { failedLogin, startLogin, successfulLogin, storeToken } from '../../redux/userSlice';
+import { useNavigate } from 'react-router-dom';
+
 const GoogleSignupDialog = ({ open, setOpen, userInfo }) => {
-  const handleClose = () => {
-    console.log({ ...userInfo, ...profileInfo });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSignup = async () => {
+    try {
+      dispatch(startLogin());
+      const res = await axiosInstance.post('/user/', {
+        email: userInfo.email,
+        first_name: userInfo.firstname,
+        last_name: userInfo.lastname,
+        password: userInfo.password,
+        ...profileInfo,
+      });
+      let { token, ...userData } = res.data;
+      dispatch(storeToken(token.access_token));
+      dispatch(successfulLogin(userData));
+      navigate('/');
+    } catch (err) {
+      dispatch(failedLogin());
+    }
+
     setOpen(false);
   };
   const [profileInfo, setProfileInfo] = useState({ gender: '', age: '', postcode: '' });
@@ -24,7 +48,7 @@ const GoogleSignupDialog = ({ open, setOpen, userInfo }) => {
           <SignUpForm2 state={[profileInfo, setProfileInfo]} />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} autoFocus>
+          <Button onClick={handleSignup} autoFocus>
             Submit
           </Button>
         </DialogActions>
