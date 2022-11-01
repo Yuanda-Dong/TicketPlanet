37e772524c83fee4fcb9185e52e875189de77a55
Dylan Oldfield <dylan.oldfield@yahoo.com>
2022-11-01 21:49:35 +1100

    updated payment models

--- backend/models/payment.py
+++ backend/models/payment.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import uuid
 from typing import Optional, List, Dict
 from enum import Enum
-from pydantic import BaseModel, Field, validator
+from pydantic import AnyUrl, BaseModel, Field, validator
 
 
 
@@ -19,6 +19,8 @@ class TicketPayment(BaseModel):
     base_ticket:str = Field(...)
     user_id: str = Field(...)
     quantity:int = Field(...)
+    seat:Optional[int]
+    section: Optional[str]
     @validator('quantity')
     def quant_must_be_positive(cls, v):
         if v <= 0:
@@ -47,14 +49,49 @@ class TicketPayment(BaseModel):
 #         return v
 
 
-class TicketPaymentIntent(BaseModel):
-    amount: int
-    currency: str = 'aud'
-    payment_method_types: List[str] = ['card']
-    metadata: TicketPayment
+
+class ProductData(BaseModel):
+    name: str 
+    description: Optional[str]
+    images: Optional[List[AnyUrl]]
     
+
+class PriceData(BaseModel):
+    currency: str = 'aud'
+    product_data: ProductData
+    unit_amount: int #remember this is in cents
+    @validator('unit_amount')
+    def price_floor(cls, v):
+        if v < 20:
+            raise ValueError('minimum price is 20c')
+        return v
+    
+class PaymentMode(str, Enum):
+    payment='payment'
+    
+class LineItems(BaseModel):
+    price_data: PriceData
+    quantity: int
+    @validator('quant')
+    def quant_must_be_positive(cls, v):
+        if v < 1:
+            raise ValueError('quantity must be positive')
+        return v
+    
+
+class TicketPaymentSession(BaseModel):
+    cancel_url: AnyUrl
+    success_url: AnyUrl
+    mode: PaymentMode
+    customer_email: str
 
 class PaymentIntentReturn(BaseModel):
     clientSecret: str
     #tickets: List[str] = []
     
+class TicketPaymentIntent(BaseModel):
+    amount: int
+    currency: str = 'aud'
+    payment_method_types: List[str] = ['card']
+    metadata: TicketPayment
+
