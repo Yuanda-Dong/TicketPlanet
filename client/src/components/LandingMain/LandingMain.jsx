1c12b6251e5ad6ee2121e3ef33e0a6247f69c462
Yuanda <dongyuanda@gmail.com>
2022-11-09 02:16:04 +1100

    added front-end for recommendation

--- client/src/components/LandingMain/LandingMain.jsx
+++ client/src/components/LandingMain/LandingMain.jsx
@@ -14,17 +14,23 @@ import './LandingMain.css';
 export default function LandingMain() {
   const [value, setValue] = React.useState('1');
   const { currentUser } = useSelector((state) => state.user);
+  const [recValue, setRecValue] = React.useState('1');
+  const handleChange2 = (event, newValue) => {
+    setRecValue(newValue);
+  };
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   const [events, setEvents] = useState([]);
+  const [recEvents,setRecEvents] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       let res = await axiosInstance.get('/event/upcoming?pageSize=12&pageNum=0');
       setEvents(res.data);
+      setRecEvents(res.data);
     }
     fetchData();
   }, []);
@@ -35,7 +41,7 @@ export default function LandingMain() {
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <TabList className="list" onChange={handleChange} aria-label="lab API tabs example">
             <Tab label="Upcoming Events" value="1" />
-            {currentUser && <Tab label="For You" value="2" />}
+            {currentUser && <Tab label="For You Based on " value="2" />}
           </TabList>
         </Box>
         <TabPanel value="1">
@@ -57,11 +63,45 @@ export default function LandingMain() {
           </Link>
         </TabPanel>
         <TabPanel value="2">
+          <TabContext value={recValue}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+
+          <TabList className="list" onChange={handleChange2} aria-label="lab API tabs example">
+            <Tab label="Past event types" value="1" />
+            <Tab label="Past booked hosts" value="2" />
+            <Tab label="Description" value="3" />
+            <Tab label="demographic" value="4" />
+          </TabList>
+        </Box>
+          <TabPanel value="1">
           <div className="events-container">
-            {events.map((e) => (
+            {recEvents.map((e) => (
               <EventCard key={e._id} eventInfo={e} />
             ))}
           </div>
+          </TabPanel>
+          <TabPanel value="2">
+          <div className="events-container">
+            {recEvents.map((e) => (
+              <EventCard key={e._id} eventInfo={e} />
+            ))}
+          </div>
+          </TabPanel>
+          <TabPanel value="3">
+          <div className="events-container">
+            {recEvents.map((e) => (
+              <EventCard key={e._id} eventInfo={e} />
+            ))}
+          </div>
+          </TabPanel>
+          <TabPanel value="4">
+          <div className="events-container">
+            {recEvents.map((e) => (
+              <EventCard key={e._id} eventInfo={e} />
+            ))}
+          </div>
+          </TabPanel>
+          </TabContext>
         </TabPanel>
       </TabContext>
     </Box>
