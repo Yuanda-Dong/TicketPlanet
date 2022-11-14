430ea89193b3b0773882ac39e43322fa31e8b55b
Serena71 <serena991031zs@gmail.com>
2022-11-14 15:29:40 +1100

    add cancel event api

--- client/src/components/EventList/EventList.jsx
+++ client/src/components/EventList/EventList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Menu from '@mui/material/Menu';
@@ -8,9 +8,18 @@ import Paper from '@mui/material/Paper';
 import './EventList.css';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { axiosInstance } from '../../config.js';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogActions from '@mui/material/DialogActions';
+import Button from '@mui/material/Button';
 
 const EventList = (props) => {
   const [anchoEl, setanchoEl] = React.useState(false);
+  const [DeleteOpen, setDeleteOpen] = useState(false);
+
   const handleOpenNavMenu = (event) => {
     event.preventDefault();
     setanchoEl(event.currentTarget);
@@ -19,11 +28,26 @@ const EventList = (props) => {
     setanchoEl(null);
   };
 
+  const handleClickDeleteOpen = () => {
+    setDeleteOpen(true);
+  };
+
+  const handleDeleteClose = () => {
+    setDeleteOpen(false);
+  };
+
   const publsihed_operations = [
     { id: 'Edit', to: `/edit/${props.eventInfo._id}` },
-    { id: 'Cancel', to: '/cancel-event' },
+    // { id: 'Cancel', to: '/cancel-event' },
     { id: 'View', to: `/event/${props.eventInfo._id}` },
   ];
+
+  const handleCancelEvent = async () => {
+    console.log('sneding cancel api');
+    await axiosInstance.post(`/event/cancel/${props.eventInfo._id}`);
+    // setanchoEl(null);
+    handleDeleteClose();
+  };
   const event_menu_id = props.eventInfo._id;
   const event_menu = (
     <Menu
@@ -48,6 +72,31 @@ const EventList = (props) => {
           </MenuItem>
         </Link>
       ))}
+      <MenuItem>
+        <Typography onClick={handleClickDeleteOpen} textAlign="center">
+          Cancel
+        </Typography>
+      </MenuItem>
+
+      <Dialog
+        open={DeleteOpen}
+        onClose={handleDeleteClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{'Cancel Booking'}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you need to cancel this event and process all refunds?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDeleteClose}>Disagree</Button>
+          <Button onClick={handleCancelEvent} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Menu>
   );
   return (
